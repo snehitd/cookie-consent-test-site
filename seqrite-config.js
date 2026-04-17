d963c2d4a2fc3c1109ed9426a8671d5000ce4a4c
snehit.deokar <snehit.deokar@quickheal.com>
2026-04-17 13:42:41 +0530

    Markupbased enforcement

---
+++ seqrite-config.js
@@ -0,0 +1,205 @@
+
+let userLang = (navigator.language || (navigator).userLanguage || 'en');
+console.log("Browser Language Code: ", userLang);
+window.klaroConfig = {
+  "consentManagerId": "69e153becac9fb3cdbbe01d8",
+  "uuid": "aeaceb99-60de-4c00-b1ab-eea2aa3509c5",
+  "domain": "https://devop.pre-dataprivacy.com/cmapi",
+  "version": 1,
+  "elementID": "SEQRITE",
+  lang: userLang,
+  "storageMethod": "cookie",
+  "storageName": "https://cookie-consent-enforcement-sdpdev.netlify.app/",
+  "cookieExpiresAfterDays": 365,
+  "default": true,
+  "mustConsent": false,
+  "acceptAll": true,
+  "hideDeclineAll": false,
+  "hideLearnMore": false,
+  "disablePoweredBy": false,
+  "translations": {
+    "english": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>"
+      }
+    },
+    "en": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>"
+      }
+    },
+    "zz": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>"
+      }
+    }
+  },
+  "styling": {
+    "theme": [
+      "light",
+      "custom"
+    ]
+  },
+  "htmlTexts": true,
+  "embedded": false,
+  "declarative": false,
+  "groupByPurpose": false,
+  "hideToggleAll": false,
+  "appName": "testing enforce",
+  "description": "We respects your privacy and is committed to protecting your personal data.",
+  "companyName": "testing enforce",
+  "companyAddress": " ",
+  "services": [
+    {
+      "name": "analytics",
+      "title": "Analytics",
+      "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>",
+      "purposes": [],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_ga",
+        "_ga_89RWJTCPLZ"
+      ]
+    },
+    {
+      "name": "others",
+      "title": "Others",
+      "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>",
+      "purposes": [],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "marketing_id"
+      ]
+    }
+  ]
+};
+showCookie();
