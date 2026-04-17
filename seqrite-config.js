f841dd6c2006adad3a16748292b3f0c04cf33caa
snehit.deokar <snehit.deokar@quickheal.com>
2026-04-17 15:43:04 +0530

    Markupbased enforcement 3

--- seqrite-config.js
+++ seqrite-config.js
@@ -2,16 +2,16 @@
 let userLang = (navigator.language || (navigator).userLanguage || 'en');
 console.log("Browser Language Code: ", userLang);
 window.klaroConfig = {
-  "consentManagerId": "69e153becac9fb3cdbbe01d8",
-  "uuid": "aeaceb99-60de-4c00-b1ab-eea2aa3509c5",
+  "consentManagerId": "69df30911fe1fa0d2a5e84cb",
+  "uuid": "2801b6b0-4f1d-45b8-b061-1470d66d31b1",
   "domain": "https://devop.pre-dataprivacy.com/cmapi",
   "version": 1,
   "elementID": "SEQRITE",
   lang: userLang,
   "storageMethod": "cookie",
-  "storageName": "https://cookie-consent-enforcement-sdpdev.netlify.app/",
+  "storageName": "https://cookie-consent-enforcement-sdpdev.netlify.app",
   "cookieExpiresAfterDays": 365,
-  "default": true,
+  "default": false,
   "mustConsent": false,
   "acceptAll": true,
   "hideDeclineAll": false,
@@ -55,13 +55,13 @@ window.klaroConfig = {
         "service": "Cookie",
         "services": "Cookies"
       },
-      "analytics": {
-        "title": "Analytics",
-        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
-      },
       "others": {
         "title": "Others",
-        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>"
+        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">4 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
       }
     },
     "en": {
@@ -101,13 +101,13 @@ window.klaroConfig = {
         "service": "Cookie",
         "services": "Cookies"
       },
-      "analytics": {
-        "title": "Analytics",
-        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
-      },
       "others": {
         "title": "Others",
-        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>"
+        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">4 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
       }
     },
     "zz": {
@@ -147,13 +147,13 @@ window.klaroConfig = {
         "service": "Cookie",
         "services": "Cookies"
       },
-      "analytics": {
-        "title": "Analytics",
-        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
-      },
       "others": {
         "title": "Others",
-        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>"
+        "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">4 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>"
       }
     }
   },
@@ -168,36 +168,39 @@ window.klaroConfig = {
   "declarative": false,
   "groupByPurpose": false,
   "hideToggleAll": false,
-  "appName": "testing enforce",
+  "appName": "SDP Dev Cookie Enforcement",
   "description": "We respects your privacy and is committed to protecting your personal data.",
-  "companyName": "testing enforce",
+  "companyName": "SDP Dev Cookie Enforcement",
   "companyAddress": " ",
   "services": [
     {
-      "name": "analytics",
-      "title": "Analytics",
-      "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>",
+      "name": "others",
+      "title": "Others",
+      "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">4 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga_89RWJTCPLZ</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>",
       "purposes": [],
       "required": false,
-      "default": true,
+      "default": false,
       "optOut": false,
       "onlyOnce": true,
       "cookies": [
-        "_ga",
+        "marketing_id",
+        "_ga_89RWJTCPLZ",
+        "marketing_id",
         "_ga_89RWJTCPLZ"
       ]
     },
     {
-      "name": "others",
-      "title": "Others",
-      "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">1 Cookie</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;marketing_id</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Other Cookies</span></div></div></details>",
+      "name": "analytics",
+      "title": "Analytics",
+      "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0;list-style:disclosure-closed\">2 Cookies</summary><div style=\"margin-top:6px\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><span><b>Name:</b>&nbsp;_ga</span>&emsp;<span><b>Host:</b>&nbsp;cookie-consent-enforcement-sdpdev.netlify.app</span>&emsp;<span><b>Service:</b>&nbsp;Google Analytics</span></div></div></details>",
       "purposes": [],
       "required": false,
-      "default": true,
+      "default": false,
       "optOut": false,
       "onlyOnce": true,
       "cookies": [
-        "marketing_id"
+        "_ga",
+        "_ga"
       ]
     }
   ]
